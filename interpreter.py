a8009ab49770e8519f23a0d7d666664d07b6ec21
jiachongliu <jcl314159@gmail.com>
2017-02-09 00:14:21 -0800

    python
    python interpreter

---
+++ interpreter.py
@@ -0,0 +1,43 @@
+a, b = input(""), input("")
+
+
+what_to_execute = {
+        "instructions": [("LOAD_VALUE", 0),
+                         ("LOAD_VALUE", 1),
+                         ("ADD_TWO_VALUES", None),
+                         ("PRINT_ANSWER", None)],
+        "numbers": [a, b]
+        }
+class Interpreter:
+    def __init__(self):
+        self.stack = []
+    
+    def LOAD_VALUE(self, number):
+        self.stack.append(number)
+
+    def PRINT_ANSWER(self):
+        answer = self.stack.pop()
+        print(answer)
+
+    def ADD_TWO_VALUES(self):
+        first_num = self.stack.pop()
+        second_num = self.stack.pop()
+        total = first_num + second_num
+        self.stack.append(total)
+
+    def run_code(self, what_to_execute):
+        instructions = what_to_execute["instructions"]
+        numbers = what_to_execute["numbers"]
+        
+        for each_step in instructions:
+            instruction, argument = each_step
+            if instruction == "LOAD_VALUE":
+                number = numbers[argument]
+                self.LOAD_VALUE(number)
+            elif instruction == "ADD_TWO_VALUES":
+                self.ADD_TWO_VALUES()
+            elif instruction == "PRINT_ANSWER":
+                self.PRINT_ANSWER()
+
+interpreter = Interpreter()
+interpreter.run_code(what_to_execute)
